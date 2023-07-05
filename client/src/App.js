a09a39d63c2150d3a133c1b1afe2644cead2680a
nina benarbia <nadinebenarbia@gmail.com>
2023-07-05 18:24:45 +0100

    logIn / register front

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Home from './components/pages/Home';
 import UserProfile from './components/pages/Profile';
+import Register from './components/Auth/Register/Register';
+import LogIn from './components/Auth/LogIn/LogIn';
+import axios from 'axios';
 
-const App = () => (
-  <BrowserRouter>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<UserProfile />} />
-    </Routes>
-  </BrowserRouter>
-);
+const App = () => {
+  const [userData, setUserData] = useState([]);
+   
+  useEffect (()=>{
+   const getAllUsers =()=>{
+    axios.get('http://localhost:3000/users')
+    .then((res)=>{
+      setUserData(res.userData)
+      console.log(userData)
+    })
+    .catch ((err)=>{
+      console.log(err)
+    })
+   }
+   getAllUsers()
+  },[])
+
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/profile" element={<UserProfile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- client/src/components/Auth/Register/Register.jsx
+++ client/src/components/Auth/Register/Register.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-export default Register
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/register", { userName, email, password })
+      .then((response) => {
+        console.log("User added");
+        
+      })
+      .catch((err) => {
+        setError("Error adding the User");
+        console.error(err);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Register new Employee</h1>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Full Name:</label>
+          <input
+            type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
