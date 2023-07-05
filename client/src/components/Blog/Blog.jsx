fb3b8b83b572c1ef03af073c64ca832b10b42352
yacineamrouche <yacineamrouchr03@gmail.com>
2023-07-05 18:26:17 +0100

    yacin

--- client/src/components/Blog/Blog.jsx
+++ client/src/components/Blog/Blog.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import App from "../../App";
+import PostDetails from "./PostDetails";
+
 
 const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
-}
+  const [data, setData] = useState([]);
+  const [trigger, setTrigger] = useState(false);
+  const [image, setImage] = useState("");
 
-export default Blog
+  const setFileTobse = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setImage(reader.result);
+    };
+  };
+
+  const handleimage = (e) => {
+    const file = e.target.file[0];
+    setFileTobse(file);
+    console.log(file);
+  };
+
+  useEffect(() => {
+    fetch();
+    setTrigger(false);
+  }, [trigger]);
+
+  const fetch = () => {
+    axios
+      .get("http://localhost:3000/blogs")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return <div>
+    <PostDetails data={data} setTrigger={setTrigger}/>
+  </div>;
+};
+
+export default Blog;
