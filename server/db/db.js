dc4bce95ac29ea2e5c99005abc25291d5624ced2
nina benarbia <nadinebenarbia@gmail.com>
2023-07-06 09:49:53 +0100

    fix problm

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 
-const sequelize = new Sequelize('hrm', 'root', 'yacine251203**', {
+const sequelize = new Sequelize('hrm', 'root', 'root', {
 
   host: 'localhost',
   dialect: 'mysql',
