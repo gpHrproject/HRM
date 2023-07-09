607cfa29fac1c05d233bf37ac1ecd33a5bb65a8b
nina benarbia <nadinebenarbia@gmail.com>
2023-07-09 07:55:20 +0100

    nadine

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 
 
-const sequelize = new Sequelize('hrm', 'mohamedali', 'borjelamri2014', {
+const sequelize = new Sequelize('hrm', 'root', 'root', {
 
 
   host: 'localhost',
