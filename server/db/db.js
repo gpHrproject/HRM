266cf65279313892dc5cdb095f6a47ed81c30813
Mohamed9820m <mohamedhabibmarouani8@gmail.com>
2023-07-09 04:49:59 +0100

    done

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 
 
-const sequelize = new Sequelize('hrm', 'root', 'root', {
+const sequelize = new Sequelize('hrm', 'root', 'admin', {
 
 
   host: 'localhost',
